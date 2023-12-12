18aae06f004b976e0002c58050ca73b4d19a07ed
wyao <willjarvin@gmail.com>
2023-12-12 17:48:31 +0800

    feat: initial commit and finish the basic java web server

---
+++ src/server/HttpServer.java
@@ -0,0 +1,49 @@
+package server;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer();
+        server.await();
+    }
+
+    private void await() {
+        ServerSocket serverSocket = null;
+        int port = 8080;
+        try {
+            serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (true) {
+            Socket socket = null;
+            InputStream input = null;
+            OutputStream output = null;
+            try {
+                socket = serverSocket.accept();
+                input = socket.getInputStream();
+                output = socket.getOutputStream();
+                Request request = new Request(input);
+                request.parse();
+                Response response = new Response(output);
+                response.setRequest(request);
+                response.sendStaticResource();
+                socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
