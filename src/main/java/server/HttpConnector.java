e39f16b1074451beab76087de951d2268f6f0ccb
wyao <willjarvin@gmail.com>
2023-12-18 13:09:08 +0800

    feat: add processor pool

--- src/main/java/server/HttpConnector.java
+++ src/main/java/server/HttpConnector.java
@@ -5,8 +5,14 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class HttpConnector implements Runnable {
+    int minProcessors = 3;
+    int maxProcessors = 10;
+    int curProcessors = 0;
+    Deque<HttpProcessor> processors = new ArrayDeque<>();
     @Override
     public void run() {
         ServerSocket serverSocket = null;
@@ -18,21 +24,58 @@ public class HttpConnector implements Runnable {
             System.exit(1);
         }
 
+        for (int i = 0; i < minProcessors; i++) {
+            HttpProcessor initProcessor = new HttpProcessor(this);
+            initProcessor.start();
+            processors.push(initProcessor);
+        }
+        curProcessors = minProcessors;
+
         while (true) {
             Socket socket = null;
             try {
                 socket = serverSocket.accept();
-                HttpProcessor processor = new HttpProcessor();
-                processor.process(socket);
-                socket.close();
+                HttpProcessor processor = createProcessor();
+                if (processor == null) {
+                    socket.close();
+                    continue;
+                }
+                processor.assign(socket);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
+    private HttpProcessor createProcessor() {
+        synchronized (processors) {
+            if (processors.size() > 0) {
+                return processors.pop();
+            }
+
+            if (curProcessors < maxProcessors) {
+                return (newProcessor());
+            } else {
+                return null;
+            }
+            
+        }
+    }
+
+    private HttpProcessor newProcessor() {
+        HttpProcessor initProcessor = new HttpProcessor(this);
+        initProcessor.start();
+        processors.push(initProcessor);
+        curProcessors ++;
+        return processors.pop();
+    }
+
     public void start() {
         Thread thread = new Thread(this);
         thread.start();
+    }
+
+    public void recycle(HttpProcessor processor) {
+        processors.push(processor);
     }
 }
--- src/main/java/server/HttpProcessor.java
+++ src/main/java/server/HttpProcessor.java
@@ -4,11 +4,61 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class HttpProcessor {
-    public HttpProcessor() {
+public class HttpProcessor implements Runnable {
+    Socket socket;
+    boolean available = false;
+    HttpConnector connector;
+    public HttpProcessor(HttpConnector connector) {
+        this.connector = connector;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            Socket socket = await();
+            if (socket == null) {
+                continue;
+            }
+            process(socket);
+            connector.recycle(this);
+        }
+    }
+
+    private synchronized Socket await() {
+        while (!available) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+
+        Socket socket = this.socket;
+        available = false;
+        notifyAll();
+        return socket;
+    }
+
+    synchronized void assign(Socket socket) {
+        while (available) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+        this.socket = socket;
+        available = true;
+        notifyAll();
     }
 
     public void process(Socket socket) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         InputStream input = null;
         OutputStream output = null;
         try {
@@ -26,8 +76,14 @@ public class HttpProcessor {
                 StaticResourceProcessor processor = new StaticResourceProcessor();
                 processor.process(request, response);
             }
+            socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void start() {
+        Thread thread = new Thread(this);
+        thread.start();
     }
 }
